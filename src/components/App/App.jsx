a9d8e5828cc42675c675d4b0b419dec8980a501c
Elvin Vandamov <Elvin@Mac.localdomain>
2025-01-24 00:35:43 -0800

    changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,6 +6,7 @@ import Main from "../Main/Main";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import ItemModal from "../ItemModal/ItemModal";
 import { getWeather, filterWeatherData } from "../../utils/weatherApi";
+import Footer from "../Footer/Footer";
 
 function App() {
   const [weatherData, setWeatherData] = useState({
@@ -51,7 +52,7 @@ function App() {
         onclose={closeActiveModal}
       >
         <label htmlFor="name" className="modal__label">
-          name{" "}
+          Name{" "}
           <input
             type="text"
             className="modal__input"
@@ -60,7 +61,7 @@ function App() {
           />
         </label>
         <label htmlFor="imageUrl" className="modal__label">
-          name{" "}
+          Image{" "}
           <input
             type="text"
             className="modal__input"
@@ -72,21 +73,21 @@ function App() {
           <legend className="modal__legend">Select the weather type:</legend>
           <label htmlFor="hot" className="modal__label modal__label_type_radio">
             <input id="hot" type="radio" className="modal__radio-input" />
-            Hot
+            <span className="modal__radio-text">Hot</span>
           </label>
           <label
             htmlFor="warm"
             className="modal__label modal__label_type_radio"
           >
             <input id="warm" type="radio" className="modal__radio-input" />
-            Warm
+            <span className="modal__radio-text">Warm</span>
           </label>
           <label
             htmlFor="cold"
             className="modal__label modal__label_type_radio"
           >
             <input id="cold" type="radio" className="modal__radio-input" />
-            Cold
+            <span className="modal__radio-text">Cold</span>
           </label>
         </fieldset>
       </ModalWithForm>
@@ -95,6 +96,7 @@ function App() {
         card={selecteCard}
         onClose={closeActiveModal}
       />
+      <Footer />
     </div>
   );
 }
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -5,9 +5,11 @@ function ModalWithForm({ children, buttonText, title, activeModal, onclose }) {
     <div className={`modal ${activeModal === "add-garment" && "modal_opened"}`}>
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
-        <button onClick={onclose} type="button" className="modal__close">
-          CLOSE
-        </button>
+        <button
+          onClick={onclose}
+          type="button"
+          className="modal__close modal__close_form"
+        ></button>
         <form className="modal__form">
           {children}
           <button type="submit" className="modal__submit">
